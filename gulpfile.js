d5ebd4821dc5b0416e7a6063e593f929256732b8
Contra <contra@maricopa.edu>
2014-07-11 23:08:19 -0700

    first pass

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp');
+var lr = require('gulp-livereload');
+var gif = require('gulp-if');
+var lr = require('gulp-livereload');
+var cached = require('gulp-cached');
+var uglify = require('gulp-uglify');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var jeet = require('jeet');
+var autoprefixer = require('autoprefixer-stylus');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
+var globs = {
+  js: ['data/**/*', 'src/js/**/*.js'],
+  css: 'src/css/**/*.styl',
+  static: ['src/**/*', '!src/css/**/*.styl', '!src/js/script.js']
+};
+
+gulp.task('watch', function(){
+  gulp.watch(globs.css, ['css']);
+  gulp.watch(globs.js, ['js']);
+});
+
+gulp.task('static', function(){
+  return gulp.src(globs.static)
+    .pipe(cached('build'))
+    .pipe(gif('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+    .pipe(lr());
+});
+
+gulp.task('js', function(){
+  return browserify('./src/js/script.js')
+    .bundle()
+    .pipe(source('script.js'))
+    .pipe(buffer())
+    .pipe(cached('js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(lr());
+});
+
+gulp.task('css', function(){
+  return gulp.src('src/css/**/*.styl')
+    .pipe(cached('css'))
+    .pipe(stylus({
+      use:[
+        nib(),
+        jeet(),
+        autoprefixer()
+      ]
+    }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(lr());
+});
+
+gulp.task('default', ['css', 'js', 'static', 'watch']);
---
+++ scraper.js
@@ -0,0 +1,123 @@
+var request = require('request');
+var geojson = require('geojson');
+var cheerio = require('cheerio');
+var _ = require('lodash');
+var fs = require('fs');
+var path = require('path');
+
+var outPath = path.join(__dirname, './data/');
+var jsonOut = path.join(outPath, 'businesses.json');
+var geojsonOut = path.join(outPath, 'businesses-geojson.json');
+
+getResults(function(err, docs){
+  if (err) {
+    return console.error(err);
+  }
+  fs.writeFileSync(jsonOut, JSON.stringify(docs, null, 2));
+
+  geojson.parse(docs, {Point: ['lat', 'lon']}, function(out){
+    fs.writeFileSync(geojsonOut, JSON.stringify(out, null, 2));
+
+    console.log('Done!', docs.length, 'polluting businesses');
+  });
+});
+
+function getResults(cb) {
+  var url = 'http://www.azdeq.gov/cgi-bin/databases/eb.pl';
+  var rOpt = {
+    url: url,
+    method: 'post',
+    form: {
+      pagenum: 1,
+      cgifunction: 'Search'
+    }
+  };
+
+  request(rOpt, function(err, res, body){
+    if (err) {
+      return cb(err);
+    }
+    if (res.statusCode !== 200) {
+      return cb(new Error(body));
+    }
+
+    cb(null, resultsToJSON(body));
+  });
+}
+
+function resultsToJSON(body) {
+  var $ = cheerio.load(body);
+  var items = $('table').toArray();
+
+  var data = _(items)
+    .map(tableToJSON)
+    .filter(isHeader)
+    .filter(hasLocation)
+    .groupBy('placeLatLong')
+    .map(cleanupDocument)
+    .valueOf();
+
+  return data;
+}
+
+function tableToJSON(el) {
+  var $ = cheerio;
+  var fields = $(el).children('tr').toArray();
+  var fieldValues = _.map(fields, function(el){
+    var parts = $(el).children('td').toArray();
+    var partValues = _.map(parts, function(el){
+      return $(el).text();
+    });
+    if (partValues[1]) {
+      return [camelize(partValues[0]), partValues[1]];
+    }
+  });
+  return _.zipObject(fieldValues);
+}
+
+function isHeader(doc) {
+  return doc.creditType !== 'Credit Amount';
+}
+
+function hasLocation(doc) {
+  return !!doc.placeLatLong && doc.placeLatLong !== 'Unknown';
+}
+
+function cleanupDocument(docs){
+  var out = {};
+
+  // coordinates
+  var coords = docs[0].placeLatLong.split(' / ');
+  out.lat = parseFloat(coords[0]);
+  out.lon = parseFloat(coords[1]);
+
+  // contact
+  var contact = docs[0].contactNameAndPhone.split('; ');
+  if (contact[0] !== 'Unknown') {
+    out.contact = {
+      name: contact[0],
+      phone: contact[1]
+    };
+  }
+  out.company = docs[0].companyName;
+  out.name = docs[0].placeName;
+  out.address = docs[0].placeAddress;
+
+  // meta
+  out.pollutants = docs.map(function(doc){
+    return {
+      pollutant: doc.pollutant,
+      since: new Date(doc.reductionDate)
+    };
+  });
+
+  return out;
+}
+
+function camelize(str) {
+  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function(letter, index) {
+    return index === 0 ? letter.toLowerCase() : letter.toUpperCase();
+  })
+  .replace(/\s+/g, '')
+  .replace(/\//g, '');
+}
